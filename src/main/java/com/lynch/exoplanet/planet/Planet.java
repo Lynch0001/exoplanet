4efae53e3234b5224bc164c25d2d6e8f5aa2f776
Tim Lynch <lynch0001@gmail.com>
2020-05-14 14:31:23 -0400

    Complete Model/Entity build

--- src/main/java/com/lynch/exoplanet/planet/Planet.java
+++ src/main/java/com/lynch/exoplanet/planet/Planet.java
@@ -5,6 +5,8 @@ import com.lynch.exoplanet.star.Star;
 import javax.persistence.*;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import java.util.Date;
+
 @SuppressWarnings("JpaDataSourceORMInspection")
 @Entity
 @Table(name="planettable")
@@ -17,8 +19,83 @@ public class Planet {
 	@Column(name="pl_hostname")
 	private String plHostName;
 
+	@Column(name="pl_letter")
+	private String plLetter;
+
 	@Column(name="pl_name")
 	private String plName;
+
+	@Column(name="pl_discmeth")
+	private String plDiscMeth;
+
+	@Column(name="pl_orbper")
+	private Double plOrbPer;
+
+	@Column(name="pl_orbsmax")
+	private Double plOrbSMAx;
+
+	@Column(name="pl_orbeccen")
+	private Double plOrbEccen;
+
+	@Column(name="pl_orbincl")
+	private Double plOrbIncl;
+
+	@Column(name="pl_radj")
+	private Double plRadJ;
+
+	@Column(name="pl_dens")
+	private Double plDens;
+
+	@Column(name="rowupdate")
+	private Date rowUpdate;
+
+	@Column(name="pl_eqt")
+	private Double plEqT;
+
+	@Column(name="pl_massj")
+	private Double plMassJ;
+
+	@Column(name="pl_masse")
+	private Double plMassE;
+
+	@Column(name="pl_rade")
+	private Double plRadE;
+
+	@Column(name="pl_rads")
+	private Double plRadS;
+
+	@Column(name="pl_trandur")
+	private Double plTranDur;
+
+	@Column(name="pl_ratdor")
+	private Double plRatDor;
+
+	@Column(name="pl_ratror")
+	private Double plRatRor;
+
+	@Column(name="pl_disc")
+	private Integer plDisc;
+
+	@Column(name="pl_disc_reflink")
+	private String plDiscRefLink;
+
+	@Column(name="pl_locale")
+	private String plLocale;
+
+	@Column(name="pl_facility")
+	private String plFacility;
+
+	@Column(name="pl_telescope")
+	private String plTelescope;
+
+	@Column(name="pl_pelink")
+	private String plPELink;
+
+	@Column(name="pl_edelink")
+	private String plEDELink;
+
+	@Column(name="pl_publ_date")
+	private String plPubDate;
 
 	@ManyToOne
 	@JoinColumn(name="star_id", nullable=false, updatable=false)
@@ -28,11 +105,36 @@ public class Planet {
 	public Planet() {
 	}
 
-	public Planet(Long id, String hname, String pname) {
-		super();
+	public Planet(Long id, String plHostName, String plLetter, String plName, String plDiscMeth, Double plOrbPer, Double plOrbSMAx, Double plOrbEccen, Double plOrbIncl, Double plRadJ, Double plDens, Date rowUpdate, Double plEqT, Double plMassJ, Double plMassE, Double plRadE, Double plRadS, Double plTranDur, Double plRatDor, Double plRatRor, Integer plDisc, String plDiscRefLink, String plLocale, String plFacility, String plTelescope, String plPELink, String plEDELink, String plPubDate, Star star) {
 		this.id = id;
 		this.plHostName = plHostName;
+		this.plLetter = plLetter;
 		this.plName = plName;
+		this.plDiscMeth = plDiscMeth;
+		this.plOrbPer = plOrbPer;
+		this.plOrbSMAx = plOrbSMAx;
+		this.plOrbEccen = plOrbEccen;
+		this.plOrbIncl = plOrbIncl;
+		this.plRadJ = plRadJ;
+		this.plDens = plDens;
+		this.rowUpdate = rowUpdate;
+		this.plEqT = plEqT;
+		this.plMassJ = plMassJ;
+		this.plMassE = plMassE;
+		this.plRadE = plRadE;
+		this.plRadS = plRadS;
+		this.plTranDur = plTranDur;
+		this.plRatDor = plRatDor;
+		this.plRatRor = plRatRor;
+		this.plDisc = plDisc;
+		this.plDiscRefLink = plDiscRefLink;
+		this.plLocale = plLocale;
+		this.plFacility = plFacility;
+		this.plTelescope = plTelescope;
+		this.plPELink = plPELink;
+		this.plEDELink = plEDELink;
+		this.plPubDate = plPubDate;
+		this.star = star;
 	}
 
 	public Long getId() {
@@ -51,6 +153,14 @@ public class Planet {
 		this.plHostName = plHostName;
 	}
 
+	public String getPlLetter() {
+		return plLetter;
+	}
+
+	public void setPlLetter(String plLetter) {
+		this.plLetter = plLetter;
+	}
+
 	public String getPlName() {
 		return plName;
 	}
@@ -59,8 +169,203 @@ public class Planet {
 		this.plName = plName;
 	}
 
-	public Star getStar() { return star; }
+	public String getPlDiscMeth() {
+		return plDiscMeth;
+	}
 
-	
-	
+	public void setPlDiscMeth(String plDiscMeth) {
+		this.plDiscMeth = plDiscMeth;
+	}
+
+	public Double getPlOrbPer() {
+		return plOrbPer;
+	}
+
+	public void setPlOrbPer(Double plOrbPer) {
+		this.plOrbPer = plOrbPer;
+	}
+
+	public Double getPlOrbSMAx() {
+		return plOrbSMAx;
+	}
+
+	public void setPlOrbSMAx(Double plOrbSMAx) {
+		this.plOrbSMAx = plOrbSMAx;
+	}
+
+	public Double getPlOrbEccen() {
+		return plOrbEccen;
+	}
+
+	public void setPlOrbEccen(Double plOrbEccen) {
+		this.plOrbEccen = plOrbEccen;
+	}
+
+	public Double getPlOrbIncl() {
+		return plOrbIncl;
+	}
+
+	public void setPlOrbIncl(Double plOrbIncl) {
+		this.plOrbIncl = plOrbIncl;
+	}
+
+	public Double getPlRadJ() {
+		return plRadJ;
+	}
+
+	public void setPlRadJ(Double plRadJ) {
+		this.plRadJ = plRadJ;
+	}
+
+	public Double getPlDens() {
+		return plDens;
+	}
+
+	public void setPlDens(Double plDens) {
+		this.plDens = plDens;
+	}
+
+	public Date getRowUpdate() {
+		return rowUpdate;
+	}
+
+	public void setRowUpdate(Date rowUpdate) {
+		this.rowUpdate = rowUpdate;
+	}
+
+	public Double getPlEqT() {
+		return plEqT;
+	}
+
+	public void setPlEqT(Double plEqT) {
+		this.plEqT = plEqT;
+	}
+
+	public Double getPlMassJ() {
+		return plMassJ;
+	}
+
+	public void setPlMassJ(Double plMassJ) {
+		this.plMassJ = plMassJ;
+	}
+
+	public Double getPlMassE() {
+		return plMassE;
+	}
+
+	public void setPlMassE(Double plMassE) {
+		this.plMassE = plMassE;
+	}
+
+	public Double getPlRadE() {
+		return plRadE;
+	}
+
+	public void setPlRadE(Double plRadE) {
+		this.plRadE = plRadE;
+	}
+
+	public Double getPlRadS() {
+		return plRadS;
+	}
+
+	public void setPlRadS(Double plRadS) {
+		this.plRadS = plRadS;
+	}
+
+	public Double getPlTranDur() {
+		return plTranDur;
+	}
+
+	public void setPlTranDur(Double plTranDur) {
+		this.plTranDur = plTranDur;
+	}
+
+	public Double getPlRatDor() {
+		return plRatDor;
+	}
+
+	public void setPlRatDor(Double plRatDor) {
+		this.plRatDor = plRatDor;
+	}
+
+	public Double getPlRatRor() {
+		return plRatRor;
+	}
+
+	public void setPlRatRor(Double plRatRor) {
+		this.plRatRor = plRatRor;
+	}
+
+	public Integer getPlDisc() {
+		return plDisc;
+	}
+
+	public void setPlDisc(Integer plDisc) {
+		this.plDisc = plDisc;
+	}
+
+	public String getPlDiscRefLink() {
+		return plDiscRefLink;
+	}
+
+	public void setPlDiscRefLink(String plDiscRefLink) {
+		this.plDiscRefLink = plDiscRefLink;
+	}
+
+	public String getPlLocale() {
+		return plLocale;
+	}
+
+	public void setPlLocale(String plLocale) {
+		this.plLocale = plLocale;
+	}
+
+	public String getPlFacility() {
+		return plFacility;
+	}
+
+	public void setPlFacility(String plFacility) {
+		this.plFacility = plFacility;
+	}
+
+	public String getPlTelescope() {
+		return plTelescope;
+	}
+
+	public void setPlTelescope(String plTelescope) {
+		this.plTelescope = plTelescope;
+	}
+
+	public String getPlPELink() {
+		return plPELink;
+	}
+
+	public void setPlPELink(String plPELink) {
+		this.plPELink = plPELink;
+	}
+
+	public String getPlEDELink() {
+		return plEDELink;
+	}
+
+	public void setPlEDELink(String plEDELink) {
+		this.plEDELink = plEDELink;
+	}
+
+	public String getPlPubDate() {
+		return plPubDate;
+	}
+
+	public void setPlPubDate(String plPubDate) {
+		this.plPubDate = plPubDate;
+	}
+
+	public Star getStar() {
+		return star;
+	}
+
+	public void setStar(Star star) {
+		this.star = star;
+	}
 }
--- src/main/java/com/lynch/exoplanet/star/Star.java
+++ src/main/java/com/lynch/exoplanet/star/Star.java
@@ -14,27 +14,81 @@ public class Star {
   @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
 
-  @Column(name="star_name", nullable = false)
-  private String name;
+  @Column(name="st_name", nullable = false)
+  private String sName;
 
-  @Column(name="star_distance")
+  @Column(name="st_pnum")
+  private Integer sNumPlanets;
+
+  @Column(name="st_ra")
+  private String sRa;
+
+  @Column(name="st_dec")
+  private String sDec;
+
+  @Column(name="st_dist")
   private Double sDistance;
 
-  @Column(name="star_mass")
-  private Double sMass;
-
-  @Column(name="star_radius")
-  private Double sRadius;
+  @Column(name="st_optmag")
+  private Double sOptMag;
 
   @SuppressWarnings("JpaDataSourceORMInspection")
-  @Column(name="star_teff")
+  @Column(name="st_teff")
   private Double sTeff;
 
-  @Column(name="star_numPlanets")
-  private Integer numPlanets;
+  @Column(name="st_mass")
+  private Double sMass;
 
-  @Column(name="star_hasExoPlanets")
-  private boolean hasExoPlanets;
+  @Column(name="st_rad")
+  private Double sRadius;
+
+  @Column(name="hd_name")
+  private String hdName;
+
+  @Column(name="hip_name")
+  private String hipName;
+
+  @Column(name="st_rah")
+  private Double sRah;
+
+  @Column(name="st_glon")
+  private Double sGlon;
+
+  @Column(name="st_glat")
+  private Double sGlat;
+
+  @Column(name="st_radv")
+  private Double sRadv;
+
+  @Column(name="st_logg")
+  private Double sLogg;
+
+  @Column(name="st_lum")
+  private Double sLum;
+
+  @Column(name="st_dens")
+  private Double sDens;
+
+  @Column(name="st_metfe")
+  private Double sMetfe;
+
+  @Column(name="st_metratio")
+  private String sMetRatio;
+
+  @Column(name="st_age")
+  private Double sAge;
+
+  @Column(name="st_vsini")
+  private Double sVsini;
+
+  @Column(name="st_acts")
+  private Double sActs;
+
+  @Column(name="st_actr")
+  private Double sActr;
+
+  @Column(name="st_actlx")
+  private Double sActlx;
 
   @OneToMany(mappedBy="star")
   private final Set<Planet> planets;
@@ -43,15 +97,31 @@ public class Star {
     planets = null;
   }
 
-  public Star(Long id, String name, Double sDistance, Double sMass, Double sRadius, Double sTeff, Integer numPlanets, boolean hasExoPlanets, Set<Planet> planets) {
+  public Star(Long id, String sName, Integer sNumPlanets, Double sDistance, Double sOptMag, Double sTeff, Double sMass, Double sRadius, String hdName, String hipName, Double sRah, Double sGlon, Double sGlat, Double sRadv, Double sLogg, Double sLum, Double sDens, Double sMetfe, String sMetRatio, Double sAge, Double sVsini, Double sActs, Double sActr, Double sActlx, Set<Planet> planets) {
     this.id = id;
-    this.name = name;
+    this.sName = sName;
+    this.sNumPlanets = sNumPlanets;
     this.sDistance = sDistance;
+    this.sOptMag = sOptMag;
+    this.sTeff = sTeff;
     this.sMass = sMass;
     this.sRadius = sRadius;
-    this.sTeff = sTeff;
-    this.numPlanets = numPlanets;
-    this.hasExoPlanets = hasExoPlanets;
+    this.hdName = hdName;
+    this.hipName = hipName;
+    this.sRah = sRah;
+    this.sGlon = sGlon;
+    this.sGlat = sGlat;
+    this.sRadv = sRadv;
+    this.sLogg = sLogg;
+    this.sLum = sLum;
+    this.sDens = sDens;
+    this.sMetfe = sMetfe;
+    this.sMetRatio = sMetRatio;
+    this.sAge = sAge;
+    this.sVsini = sVsini;
+    this.sActs = sActs;
+    this.sActr = sActr;
+    this.sActlx = sActlx;
     this.planets = planets;
   }
 
@@ -63,12 +133,20 @@ public class Star {
     this.id = id;
   }
 
-  public String getName() {
-    return name;
+  public String getsName() {
+    return sName;
   }
 
-  public void setName(String name) {
-    this.name = name;
+  public void setsName(String sName) {
+    this.sName = sName;
+  }
+
+  public Integer getsNumPlanets() {
+    return sNumPlanets;
+  }
+
+  public void setsNumPlanets(Integer sNumPlanets) {
+    this.sNumPlanets = sNumPlanets;
   }
 
   public Double getsDistance() {
@@ -77,6 +155,38 @@ public class Star {
 
   public void setsDistance(Double sDistance) {
     this.sDistance = sDistance;
+  }
+
+  public String getsRa() {
+    return sRa;
+  }
+
+  public void setsRa(String sRa) {
+    this.sRa = sRa;
+  }
+
+  public String getsDec() {
+    return sDec;
+  }
+
+  public void setsDec(String sDec) {
+    this.sDec = sDec;
+  }
+
+  public Double getsOptMag() {
+    return sOptMag;
+  }
+
+  public void setsOptMag(Double sOptMag) {
+    this.sOptMag = sOptMag;
+  }
+
+  public Double getsTeff() {
+    return sTeff;
+  }
+
+  public void setsTeff(Double sTeff) {
+    this.sTeff = sTeff;
   }
 
   public Double getsMass() {
@@ -95,28 +205,132 @@ public class Star {
     this.sRadius = sRadius;
   }
 
-  public Double getsTeff() {
-    return sTeff;
+  public String getHdName() {
+    return hdName;
   }
 
-  public void setsTeff(Double sTeff) {
-    this.sTeff = sTeff;
+  public void setHdName(String hdName) {
+    this.hdName = hdName;
   }
 
-  public Integer getNumPlanets() {
-    return numPlanets;
+  public String getHipName() {
+    return hipName;
   }
 
-  public void setNumPlanets(Integer numPlanets) {
-    this.numPlanets = numPlanets;
+  public void setHipName(String hipName) {
+    this.hipName = hipName;
   }
 
-  public boolean getHasExoPlanets() {
-    return hasExoPlanets;
+  public Double getsRah() {
+    return sRah;
   }
 
-  public void setHasExoPlanets(boolean hasExoPlanets) {
-    this.hasExoPlanets = hasExoPlanets;
+  public void setsRah(Double sRah) {
+    this.sRah = sRah;
+  }
+
+  public Double getsGlon() {
+    return sGlon;
+  }
+
+  public void setsGlon(Double sGlon) {
+    this.sGlon = sGlon;
+  }
+
+  public Double getsGlat() {
+    return sGlat;
+  }
+
+  public void setsGlat(Double sGlat) {
+    this.sGlat = sGlat;
+  }
+
+  public Double getsRadv() {
+    return sRadv;
+  }
+
+  public void setsRadv(Double sRadv) {
+    this.sRadv = sRadv;
+  }
+
+  public Double getsLogg() {
+    return sLogg;
+  }
+
+  public void setsLogg(Double sLogg) {
+    this.sLogg = sLogg;
+  }
+
+  public Double getsLum() {
+    return sLum;
+  }
+
+  public void setsLum(Double sLum) {
+    this.sLum = sLum;
+  }
+
+  public Double getsDens() {
+    return sDens;
+  }
+
+  public void setsDens(Double sDens) {
+    this.sDens = sDens;
+  }
+
+  public Double getsMetfe() {
+    return sMetfe;
+  }
+
+  public void setsMetfe(Double sMetfe) {
+    this.sMetfe = sMetfe;
+  }
+
+  public String getsMetRatio() {
+    return sMetRatio;
+  }
+
+  public void setsMetRatio(String sMetRatio) {
+    this.sMetRatio = sMetRatio;
+  }
+
+  public Double getsAge() {
+    return sAge;
+  }
+
+  public void setsAge(Double sAge) {
+    this.sAge = sAge;
+  }
+
+  public Double getsVsini() {
+    return sVsini;
+  }
+
+  public void setsVsini(Double sVsini) {
+    this.sVsini = sVsini;
+  }
+
+  public Double getsActs() {
+    return sActs;
+  }
+
+  public void setsActs(Double sActs) {
+    this.sActs = sActs;
+  }
+
+  public Double getsActr() {
+    return sActr;
+  }
+
+  public void setsActr(Double sActr) {
+    this.sActr = sActr;
+  }
+
+  public Double getsActlx() {
+    return sActlx;
+  }
+
+  public void setsActlx(Double sActlx) {
+    this.sActlx = sActlx;
   }
 
   public Set<Planet> getPlanets() {
