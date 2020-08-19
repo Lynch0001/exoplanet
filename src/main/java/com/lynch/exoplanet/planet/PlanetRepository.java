c7c9ce14b5fcc840b0fe8848d10be2cbef6b9268
Tim Lynch <lynch0001@gmail.com>
2020-08-19 11:14:53 -0400

    Update database field names in Repository SQL queries - change to NASA Data(EXO-39A)

--- src/main/java/com/lynch/exoplanet/planet/PlanetRepository.java
+++ src/main/java/com/lynch/exoplanet/planet/PlanetRepository.java
@@ -7,30 +7,30 @@ import java.util.List;
 
 public interface PlanetRepository extends JpaRepository<Planet, Long> {
 
-  @Query("SELECT p FROM Planet p where p.st_dist <= 5")
-  public List<Planet> getPlanetsWithin5Parsecs();
+  @Query("SELECT p FROM Planet p where p.sy_dist <= 5")
+  List<Planet> getPlanetsWithin5Parsecs();
 
   @Query("SELECT p FROM Planet p where p.pl_dens BETWEEN 3.6 AND 13.4")
-  public List<Planet> getRockyPlanets();
+  List<Planet> getRockyPlanets();
 
   @Query("SELECT p FROM Planet p where p.pl_orbsmax BETWEEN 1 AND 2")
-  public List<Planet> get12AuPlanets();
+  List<Planet> get12AuPlanets();
 
-  @Query("SELECT p FROM Planet p where p.pl_pnum <> 1 ORDER BY p.pl_name")
-  public List<Planet> getMultiPlanetSystems();
+  @Query("SELECT p FROM Planet p where p.sy_pnum <> 1 ORDER BY p.pl_name")
+  List<Planet> getMultiPlanetSystems();
 
   @Query("SELECT p FROM Planet p where p.pl_rade BETWEEN .75 AND 1.25")
-  public List<Planet> getEarthSizePlanets();
+  List<Planet> getEarthSizePlanets();
 
   @Query("SELECT p FROM Planet p where p.pl_rade BETWEEN 1.26 AND 2.00")
-  public List<Planet> getSuperEarthSizePlanets();
+  List<Planet> getSuperEarthSizePlanets();
 
   @Query("SELECT p FROM Planet p where p.pl_name LIKE 'Kepler%'")
-  public List<Planet> getKeplerPlanets();
+  List<Planet> getKeplerPlanets();
 
   @Query("SELECT p FROM Planet p where p.pl_name LIKE 'TRAPPIST%'")
-  public List<Planet> getTrappistPlanets();
+  List<Planet> getTrappistPlanets();
 
   @Query("SELECT p FROM Planet p where p.id IN (1,2,3,4,5)")
-  public List<Planet> getNasaInterestingPlanets();
+  List<Planet> getNasaInterestingPlanets();
 }
