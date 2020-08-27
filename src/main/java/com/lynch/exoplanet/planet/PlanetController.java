8554a914b65f56bd10a3fb8db4d81859b64748ab
Tim Lynch <lynch0001@gmail.com>
2020-08-27 13:36:48 -0400

    Implements backend pagination for Planet findAll only

--- src/main/java/com/lynch/exoplanet/planet/PlanetController.java
+++ src/main/java/com/lynch/exoplanet/planet/PlanetController.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,11 +21,23 @@ public class PlanetController {
 	@Autowired
 	private PlanetService planetService;
 
+	/*  Retain while testing pagination
+
 	@GetMapping(value ="/planets")
 	public List<Planet> getAllPlanets() {
 		log.debug("Received REST request for All Planets");
 		return planetService.getAllPlanets();
 	}
+
+	 */
+
+	@GetMapping(value ="/planets")
+	public List<Planet> getAllPlanets(Pageable pageable) {
+		log.debug("Received REST request for All Planets");
+		 Page<Planet> page = planetService.getAllPlanets(pageable);
+		return page.getContent();
+	}
+
 
 	@GetMapping(value ="/planets/{id}")
 	public Optional<Planet> getPlanet(@PathVariable Long id) {
